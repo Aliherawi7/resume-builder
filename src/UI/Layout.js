0d4f7ea25089a50083793fdc7cf92b78e3678bae
Aliherawi7 <Aliherawi7@gmail.com>
2022-05-26 14:45:34 -0700

    padding right and left added

--- src/UI/Layout.js
+++ src/UI/Layout.js
@@ -4,7 +4,9 @@ function Layout(props) {
     const styles = {
         with:"100%",
         height:"100vh",
-        paddingTop:"75px"
+        paddingTop:"75px",
+        paddingRight:"30px",
+        paddingLeft: "30px"
     }
   return (
     <div className='layout' style={styles}>{props.children}</div>
