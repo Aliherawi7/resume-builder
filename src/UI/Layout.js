739ffdaf3e6dd6207cede423bd8d6551413e0e65
Aliherawi7 <Aliherawi7@gmail.com>
2022-05-27 01:14:21 -0700

    padding right and left removed

--- src/UI/Layout.js
+++ src/UI/Layout.js
@@ -5,8 +5,7 @@ function Layout(props) {
         with:"100%",
         height:"100vh",
         paddingTop:"75px",
-        paddingRight:"30px",
-        paddingLeft: "30px"
+        
     }
   return (
     <div className='layout' style={styles}>{props.children}</div>
