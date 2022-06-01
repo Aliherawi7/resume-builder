cdb03a504fdafe24d526e3219b6e656125d4b238
Aliherawi7 <Aliherawi7@gmail.com>
2022-06-02 01:05:33 +0430

    mdern template added to the list

--- src/Templates/Templates.js
+++ src/Templates/Templates.js
@@ -1,7 +1,8 @@
 import Template1 from "./Template1";
 import Template2 from "./Template2";
+import Template3 from "./Template3";
 
-const templates = [Template1, Template2]
+const templates = [Template1, Template2, Template3]
 export const templatesName = [
     {
         name: "Blueprint",
@@ -9,6 +10,10 @@ export const templatesName = [
     },
     {
         name: "Angora",
+        description:"For those who've been there and done that, the big tunas, the white whales: the Kingfish design lets you say a whole lot without saying too much."
+    },
+    {
+        name: "Modern",
         description:"For those who've been there and done that, the big tunas, the white whales: the Kingfish design lets you say a whole lot without saying too much."
     }
 ]
