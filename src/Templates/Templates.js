356f220035ab01327d0daf025eb0df6299a66ae9
Aliherawi <ALiherawi7@gmail.com>
2022-08-05 16:54:28 +0430

    fancy modern template added to the templates list

--- src/Templates/Templates.js
+++ src/Templates/Templates.js
@@ -1,8 +1,9 @@
 import Template1 from "./Template1";
 import Template2 from "./Template2";
 import Template3 from "./Template3";
+import Template4 from "./Template4";
 
-const templates = [Template1, Template2, Template3]
+const templates = [Template1, Template2, Template3, Template4]
 export const templatesName = [
     {
         name: "Blueprint",
@@ -14,6 +15,10 @@ export const templatesName = [
     },
     {
         name: "Modern",
+        description:"For those who've been there and done that, the big tunas, the white whales: the Kingfish design lets you say a whole lot without saying too much."
+    },
+    {
+        name: "Fancy Modern",
         description:"For those who've been there and done that, the big tunas, the white whales: the Kingfish design lets you say a whole lot without saying too much."
     }
 ]
