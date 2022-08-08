413cdb0bc6eb25023209fcdddb92146a73249795
Aliherawi <ALiherawi7@gmail.com>
2022-08-08 11:17:41 +0430

    Template.css file imported

---
+++ src/Templates/Template6.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Template.css"
+import "./Template6.css"
+
+function Template6() {
+  return (
+    <div className='template template6 a4'>
+        <div className='main-container'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Template6
--- src/Templates/Templates.js
+++ src/Templates/Templates.js
@@ -3,8 +3,10 @@ import Template2 from "./Template2";
 import Template3 from "./Template3";
 import Template4 from "./Template4";
 import Template5 from "./Template5";
+import Template6 from "./Template6";
 
-const templates = [Template1, Template2, Template3, Template4, Template5]
+const templates = [Template1, Template2, Template3, Template4, Template5, Template6];
+
 export const templatesName = [
     {
         name: "Blueprint",
@@ -24,6 +26,9 @@ export const templatesName = [
     },
     {
         name: "rocket",
+        description:"For those who've been there and done that, the big tunas, the white whales: the Kingfish design lets you say a whole lot without saying too much."
+    },{
+        name: "Classic ",
         description:"For those who've been there and done that, the big tunas, the white whales: the Kingfish design lets you say a whole lot without saying too much."
     }
 ]
