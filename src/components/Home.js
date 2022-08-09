422819dd34f3171b19b71a446d2d0b5a298a7513
Aliherawi <ALiherawi7@gmail.com>
2022-08-09 14:21:33 +0430

    Home UI changed completely

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,11 @@ function Home() {
     return (
         <div className="home">
             <div className="intro">
-                <h2>
-                    BUILD A RESUME
-                    THAT WILL GET YOU HIRED
-                </h2>
-                <Link to="/build/getting-started"><button>Let's Build A Resume</button>
+                <h3>BUILD</h3>
+                <h2>YOUR</h2>
+                <h1>RESUME</h1>
+                <h3>Free and Fast</h3>
+                <Link to="/build/getting-started"><button className='hvr-buzz-out'>Let's Build A Resume</button>
                 </Link>
             </div>
         </div>
