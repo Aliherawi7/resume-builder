771a72f67f492988e477a6b1e0c9652b8de25b63
Aliherawi <ALiherawi7@gmail.com>
2022-08-10 16:08:51 +0430

    Header component imported, Layout and Nav Components removed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import "./App.css"
-import Nav from './components/Nav'
+import Header from './components/Header'
 import Loading from './UI/Loading';
-import Layout from './UI/Layout';
 
 
 const Home = React.lazy(() => import('./components/Home'))
@@ -16,8 +15,7 @@ function App() {
   return (
     <Suspense fallback={<Loading />}>
       <Router>
-        <Nav />
-        <Layout>
+        <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/build/template&&color=:color&&font=:font&&template=:template" element={<Build />} />
@@ -27,7 +25,6 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/*" element={<h1 style={{ textAlign: 'center', marginTop: '60px', fontWeight: '900' }}>Not Found</h1>} />
           </Routes>
-        </Layout>
       </Router>
     </Suspense>
   );
