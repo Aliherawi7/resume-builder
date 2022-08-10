7015bd22b346403dc3bdc3dbb038d6432366fabf
Aliherawi <ALiherawi7@gmail.com>
2022-08-10 23:31:52 +0430

    PopularTemplate component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./Home.css"
 import { Link } from 'react-router-dom'
-import Templates from '../Templates/Templates.js'
+import Templates,{templatesName} from '../Templates/Templates.js'
 import example ,{templateColor} from '../Templates/example'
-import templates,{templatesName} from '../Templates/Templates.js'
+import PopularTemplate from "./PopularTemplate"
+
 
 function Home() {
     return (
@@ -18,36 +19,7 @@ function Home() {
                     </Link>
                 </div>
             </div>
-            <div className='popular-templates'>
-                {Templates.map((item) => {
-                    const temp ={template:item}
-                    const index = Templates.findIndex((input)=>{
-                        return input == item
-                    });
-                    const color = templateColor[index];
-                    console.log(index)
-                    return (
-                        <div className={'temp-item'+(index ==2 || index==3? " xlarg":" ")} key={item.name} style={{backgroundColor:color}}>
-                            {<temp.template
-                                contactInformation={example.contactInformation}
-                                skills={example.skills}
-                                summery={example.summery}
-                                experience={example.experience}
-                                education={example.education}
-                                color={color}
-                                font={"Maler"}
-                            />}
-                            <div className='details'>
-                                <h2>{templatesName[index].name}</h2>
-                                <Link to={"/build/getting-started&&color="+color.substring(1)+"&&template="+index}>
-                                <button className='browse-btn'>Browse</button>
-                                </Link>
-                            </div>
-                        </div>
-                    )
-                })}
-
-            </div>
+            <PopularTemplate  navigate={""}/>
         </main>
     )
 }
